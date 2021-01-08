8860fd81aa04f8410f5e4e7a6686217040616b77
Przemyslaw W <pwitusik77@gmail.com>
2021-01-08 16:29:45 +0100

    refactor

---
+++ G1ANT.Addon.Net/Models/IAuthenticationModel.cs
@@ -0,0 +1,9 @@
+﻿using MailKit.Net.Imap;
+
+namespace G1ANT.Addon.Net.Models
+{
+    public interface IAuthenticationModel
+    {
+        void Authenticate(ImapClient client);
+    }
+}
---
+++ G1ANT.Addon.Net/Models/SimpleAuthenticationModel.cs
@@ -0,0 +1,29 @@
+﻿using MailKit.Net.Imap;
+using System.Net;
+
+namespace G1ANT.Addon.Net.Models
+{
+    public class SimpleAuthenticationModel : IAuthenticationModel
+    {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+
+        public SimpleAuthenticationModel()
+        { }
+
+        public SimpleAuthenticationModel(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public void Authenticate(ImapClient client)
+        {
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                var credentials = new NetworkCredential(Username, Password);
+                client.Authenticate(credentials);
+            }
+        }
+    }
+}
